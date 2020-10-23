4787288b459d1b97dd9706617cdfdcc55304b5e9
asayed <ahmedsayed.9191@gmail.com>
2020-10-23 20:48:06 +0200

    Implement the arrow function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import Child from  './Child.js';
 import {num1,num2,test,test2} from './module.js';
 function App() {
+//the normal function
+function test() {
+console.log("Test !");
+}
+//the arrow function //we can remove {} in case it's only one line
+const testArrow = () => console.log("Test Arrow! "); 
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +17,8 @@ function App() {
           Hello React  {num1 + num2} {test()} {test2()}
         </p>
         <Child />
+        {test()}
+        {testArrow()}
       </header>
     </div>
   );
