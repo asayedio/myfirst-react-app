a05dae9ae40105bae92ec42752dc12ac9cc49599
asayed <ahmedsayed.9191@gmail.com>
2020-10-24 00:06:57 +0200

    Implement events

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import './App.css';
 import Child from  './Child.js';
 import {num1,num2,test,test2} from './module.js';
 function App() {
+  React.state = {
+    name:"Ahmed"
+  }
+  function handleClick(e) {
+    e.preventDefault();
+    console.log(React.state.name);
+  }
+  function handleMove(e) {
+    e.preventDefault();
+    console.log('The mouse was moved.');
+  }
+  function handleLeave(e) {
+    e.preventDefault();
+    console.log('The mouse was leaved.');
+  }
 //the normal function
 function test() {
 console.log("Test !");
 }
 //the arrow function //we can remove {} in case it's only one line
 const testArrow = () => console.log("Test Arrow! "); 
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +35,9 @@ const testArrow = () => console.log("Test Arrow! ");
         <Child />
         {test()}
         {testArrow()}
+        <button onClick={handleClick } >Click</button>
+        <button onMouseMove={handleMove } >Move</button>
+        <button onMouseLeave={handleLeave } >Leave</button>
       </header>
     </div>
   );
