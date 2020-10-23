6336a29ebc536ee06b1e21d49b0bd986692a336d
asayed <ahmedsayed.9191@gmail.com>
2020-10-23 19:56:40 +0200

    Implement import & export

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {num1,num2,test,test2} from './module.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React  {num1 + num2} {test()} {test2()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
---
+++ src/module.js
@@ -0,0 +1,9 @@
+export const num1 = 2;
+export const num2 = 8;
+export function test() {
+    console.log("React JS");
+}
+export function test2() {
+    console.log("React Js Default");
+}
+export default test2
