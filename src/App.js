41abe00a0a6767ecf708ca6666d42c2ed223e5dd
asayed <ahmedsayed.9191@gmail.com>
2020-10-24 18:05:20 +0200

    Implement conditional if

--- src/App.js
+++ src/App.js
@@ -1,74 +1,75 @@
-import React ,{ useState, Component }from 'react';
+import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Child from  './Child.js';
-import {num1,num2,test,test2} from './module.js';
-import Items from  './components/Items.js';
+import Child from './Child.js';
+import { num1, num2, test, test2 } from './module.js';
+import Items from './components/Items.js';
 class App extends Component {
   // Declare a new state variable, which we'll call "count"
-/*   const [info, changeIntialInfo] = React.useState({
-    name: 'Ahmed',
-    age: 25
-  });
-
-  function changeInfo() {
-    changeIntialInfo((prevState) => {
-      return({
-        ...prevState,
-        name: 'Mohamed'
-        ,age:30
+  /*   const [info, changeIntialInfo] = React.useState({
+      name: 'Ahmed',
+      age: 25
+    });
+  
+    function changeInfo() {
+      changeIntialInfo((prevState) => {
+        return({
+          ...prevState,
+          name: 'Mohamed'
+          ,age:30
+        });
       });
+    }
+  
+    function handleClick(e) {
+      e.preventDefault();
+      console.log(React.state.name);
+    }
+    function handleMove(e) {
+      e.preventDefault();
+      console.log('The mouse was moved.');
+    }
+    function handleLeave(e) {
+      e.preventDefault();
+      console.log('The mouse was leaved.');
+    }
+  
+  //the normal function
+  function test() {
+  console.log("Test !");
+  }
+  //the arrow function //we can remove {} in case it's only one line
+  const testArrow = () => console.log("Test Arrow! ");  */
+ /*  state = {
+    name: ''
+  } */
+  state = {
+    items: [
+      { id: 1, name: 'Ahmed', age: 20 },
+      { id: 2, name: 'Mohamed', age: 22 },
+      { id: 3, name: 'Eslam', age: 24 },
+      { id: 4, name: 'Hassan', age: 26 },
+      { id: 5, name: 'Ibrahim', age: 28 },
+      { id: 6, name: 'Zyad', age: 30 }
+    ]
+  };
+
+  textChange = (e) => {
+    this.setState({
+      name: e.target.value
     });
   }
-
-  function handleClick(e) {
+  submitForm = (e) => {
     e.preventDefault();
-    console.log(React.state.name);
+    this.setState({
+      name: 'Succesully'
+    });
   }
-  function handleMove(e) {
-    e.preventDefault();
-    console.log('The mouse was moved.');
-  }
-  function handleLeave(e) {
-    e.preventDefault();
-    console.log('The mouse was leaved.');
-  }
-
-//the normal function
-function test() {
-console.log("Test !");
-}
-//the arrow function //we can remove {} in case it's only one line
-const testArrow = () => console.log("Test Arrow! ");  */
-state = {
-  items :[
-    {id:1,name:'Ahmed',age:20},
-    {id:2,name:'Mohamed',age:22},
-    {id:3,name:'Eslam',age:24},
-    {id:4,name:'Hassan',age:26},
-    {id:5,name:'Ibrahim',age:28},
-    {id:6,name:'Zyad',age:30}
-  ]
-};
-state ={
-  name:''
-}
-textChange =(e) => {
-  this.setState({
-    name:e.target.value
-  }); 
-}
-submitForm =(e) => {
-  e.preventDefault();
-  this.setState({
-    name:'Succesully'
-  });
-}
-render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-      {/* <p>
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <p>
           Hello React  {num1 + num2} {test()} {test2()}
         </p>
         <Child />
@@ -80,28 +81,29 @@ render(){
         <button onClick={() => changeInfo()} >Change</button>
         <p>{info.name}</p>
         <p>{info.age}</p> */}
-        {/* List Array
-        <Items items={this.state.items}/> */}
-        <p>Form</p>
-      <p>{this.state.name}</p>
-        <form onSubmit={this.submitForm}>
-          {/* <input type="text" onChange={this.textChange}/>
+
+          <Items items={this.state.items} />
+
+          <p>Form</p>
+          <p>{this.state.name}</p>
+          <form onSubmit={this.submitForm}>
+            {/* <input type="text" onChange={this.textChange}/>
           <button>Submit</button> */}
-          <textarea type="text" onChange={this.textChange}/>
-          <br />
-          <select onChange={this.textChange}>
-            <option value="v1">Option 1</option>
-            <option value="v2">Option 2</option>
-            <option value="v3">Option 3</option>
-            <option value="v4">Option 4</option>
-          </select>
-          <br />
-          <input type="submit" value="send" onSubmit={this.submitForm}/>
-        </form>
-      </header>
-    </div>
-  );
-}
+            <textarea type="text" onChange={this.textChange} />
+            <br />
+            <select onChange={this.textChange}>
+              <option value="v1">Option 1</option>
+              <option value="v2">Option 2</option>
+              <option value="v3">Option 3</option>
+              <option value="v4">Option 4</option>
+            </select>
+            <br />
+            <input type="submit" value="send" onSubmit={this.submitForm} />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,24 +1,24 @@
-import React ,{Component} from 'react';
+import React, { Component } from 'react';
 
-class Items extends React.Component{
-    render(){
+class Items extends Component {
+    render() {
         //const {id,name,age} = this.props;
         const theItems = this.props.items;
-        const items = theItems.map( (item) =>{
-            return(
+        const items = theItems.map((item) => {
+            return item.age > 25 ? (
                 <div key={item.id}>
-                    <div>{item.id}</div>
-                    <div>{item.name}</div>
-                    <div>{item.age}</div>
-                    <hr/>
-                </div>
-            )
+                <div>{item.id}</div>
+                <div>{item.name}</div>
+                <div>{item.age}</div>
+
+            </div>
+            ) : null
         })
-        return(
+        return (
             <div>
                 <div>{items}</div>
             </div>
         )
     }
 }
-export default Items;
+export default Items;	
